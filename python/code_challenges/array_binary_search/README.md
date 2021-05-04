7521c940c0e3849796483648ff3230c3d0dc9e60
HishamKhalil1990 <hisham.khalil1990@gmail.com>
2021-05-05 02:54:06 +0300

    code challenge 3 is done

---
+++ python/code_challenges/array_binary_search/README.md
@@ -0,0 +1,12 @@
+# find a value index in a list
+### it is a quick program to find the index of a value inside a list
+
+## Challenge
+### the challenge was search for the index without iterating on all values in the list
+
+## Approach & Efficiency
+### first i found the middle of the list, using if statemant, spliting the list into two halfs and recursion approach the code get the index
+### Big O is leniar
+
+## Solution
+![solution](https://i.ibb.co/5BHW4cC/Untitled-5.jpg)
--- python/tests/test_array_binary_search.py
+++ python/tests/test_array_binary_search.py
@@ -1,5 +1,35 @@
 from code_challenges.array_binary_search.array_binary_search import __version__
-
+from code_challenges.array_binary_search.array_binary_search.array_binary_search import find_index
 
 def test_version():
     assert __version__ == '0.1.0'
+
+def test1():
+    expected = 2
+    actual = find_index([4,8,15,16,23,42], 15)
+    assert actual == expected
+
+def test2():
+    expected = -1
+    actual = find_index([11,22,33,44,55,66,77], 90)
+    assert actual == expected
+
+def test3():
+    expected = -1
+    actual = find_index([1, 2, 3, 5, 6, 7], 4)
+    assert actual == expected
+
+def test4():
+    expected = 6
+    actual = find_index([4,8,9,10,15,16,23,42,50], 23)
+    assert actual == expected
+
+def test5():
+    expected = -1
+    actual = find_index([11,22,33,44,55,66,77,88,99], 3)
+    assert actual == expected
+
+def test6():
+    expected = 5
+    actual = find_index([1, 2, 3, 5, 6, 7, 8, 10, 11], 7)
+    assert actual == expected
