ad498c902a4b42450b993cfee95c130cad988d1a
HishamKhalil1990 <hisham.khalil1990@gmail.com>
2021-05-07 14:22:11 +0300

    data-structure folder is added

--- python/tests/test_linked_list.py
+++ python/tests/test_linked_list.py
@@ -1,5 +1,5 @@
 import pytest
-from code_challenges.linked_list.linked_list.linked_list import Node,Linked_List
+from code_challenges.Data_Structures.linked_list.linked_list.linked_list import Node,Linked_List
 
 
 def test_import():
