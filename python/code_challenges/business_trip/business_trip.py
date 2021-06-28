55fdc26269c8e9de8b71ef1c8d47613774bed262
HishamKhalil1990 <hisham.khalil1990@gmail.com>
2021-06-29 01:33:18 +0300

    challenge 37 is done

--- python/code_challenges/business_trip/business_trip.py
+++ python/code_challenges/business_trip/business_trip.py
@@ -1,20 +1,47 @@
-def business_trip(graph, arr):
-  try:
-    total = 0
-    def check_neighbors(city1, city2, graph):
-      cost = 0
-      node_li = []
-      nodes = graph.get_nodes()
-      for node in nodes:
-        if city1 == node.value:
-          node_li.append(node)
-      node1 = node_li[0]
-      for i in range(len(graph.get_neighbors(node1))):
-        if city2 == graph.get_neighbors(node1)[i].vertex.value:
-          cost += graph.get_neighbors(node1)[i].weight
-      return cost
-    for i in range(len(arr)-1):
-      total += check_neighbors(arr[i], arr[i+1], graph)
-    return total
-  except:
-    return None
+from ..graph.graph import Graph
+
+def business_trip(graph, city_arr):
+    node_list = graph.get_nodes()
+    node_dict = {}
+    weight = 0
+    for node in node_list:
+        node_dict[node.value] = node
+    for index in range(len(city_arr) - 1):
+        first_city = city_arr[index]
+        second_city = city_arr[index + 1]
+        first_node = node_dict[first_city]
+        second_node = node_dict[second_city]
+        first_city_neighbors = graph.get_neighbors(first_node)
+        linked = False
+        for edge in first_city_neighbors:
+            if edge.vertex == second_node:
+                weight += edge.weight
+                linked = True
+        if not linked:
+            return None
+    return weight
+
+if __name__ == "__main__":
+    g = Graph()
+    node1 = g.add_node('Pandora')
+    node2 = g.add_node('Arendelle')
+    node3 = g.add_node('Metroville')
+    node4 = g.add_node('Monstropolis')
+    node5 = g.add_node('Narnia')
+    node6 = g.add_node('Naboo')
+    g.add_edge(node1, node2, 150)
+    g.add_edge(node1, node3, 82)
+    g.add_edge(node2, node3, 99)
+    g.add_edge(node2, node4, 42)
+    g.add_edge(node3, node4, 105)
+    g.add_edge(node3, node5, 37)
+    g.add_edge(node3, node6, 26)
+    g.add_edge(node4, node6, 73)
+    g.add_edge(node5, node6, 250)
+    print(business_trip(g,['Metroville','Pandora']))
+    print('new---------------------------------')
+    print(business_trip(g,['Arendelle','Monstropolis','Naboo']))
+    print('new---------------------------------')
+    print(business_trip(g,['Naboo','Pandora']))
+    print('new---------------------------------')
+    print(business_trip(g,['Narnia','Arendelle','Naboo']))
